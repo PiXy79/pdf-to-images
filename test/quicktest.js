8c9c063f5fa2cced04b5e12548c5604e870912fa
unknown <amoriconi@amoriconi-nb.expertsystem.local>
2017-02-15 14:00:35 +0100

    Add test.pdf with multiple pages

--- test/quicktest.js
+++ test/quicktest.js
@@ -1,61 +1,56 @@
-var pdf2images = require("../lib/pdf2images.js");
-var fs = require("fs");
 
-var projectPath = __dirname.split("\\");
+var pdf2images = require('../lib/pdf2images.js');
+var fs = require('fs');
+
+var projectPath = __dirname.split('\\');
 projectPath.pop();
-projectPath = projectPath.join("\\");
+projectPath = projectPath.join('\\');
 
-var gsPath = projectPath + "\\executables\\ghostScript";
+var gsPath = projectPath + '\\executables\\ghostScript';
 
 // Rewrite the ghostscript path
 pdf2images.ghostscriptPath = gsPath;
 
-// Most simple example
-pdf2images.convert(__dirname + "/example.pdf", {}, function(resp){
+// Test multiple page PDF
+pdf2images.convert(__dirname + '/test.pdf', {}, function(resp){
 		if(!resp.success)
 		{
-			console.log("Something went wrong: " + resp.error); 
+			console.log('Something went wrong: ' + resp.error); 
 			return;
 		}
 
-		var pageCount = resp.images.length;
-		console.log('generated ' + pageCount + ' images..');
-
-		var count = 0;
+		var imageCount = 0;
 		resp.images.forEach(function(data) {
-			fs.writeFile("test/example_" + count + ".png", data, function(err) {
+			fs.writeFile('test/test_' + imageCount + '.png', data, function(err) {
 				if(err) {
 					console.log(err);
 				}
 				else {
-					console.log("The file was saved!");
+					//console.log('The file was saved!');
 				}
 			});
-			count++;
+			imageCount++;
 		});
 });
 
 // Example using a local ghostscript installation
-pdf2images.convert(__dirname + "/example.pdf", { useLocalGhostscript: true }, function(resp){
+pdf2images.convert(__dirname + '/example.pdf', { useLocalGhostscript: true }, function(resp){
 		if(!resp.success)
 		{
-			console.log("Something went wrong: " + resp.error); 
+			console.log('Something went wrong: ' + resp.error); 
 			return;
 		}
 
-		var pageCount = resp.images.length;
-		console.log('generated ' + pageCount + ' images..');
-
-		var count = 0;
+		var imageCount = 0;
 		resp.images.forEach(function(data) {
-			fs.writeFile("test/example_" + count + ".png", data, function(err) {
+			fs.writeFile('test/example_' + imageCount + '.png', data, function(err) {
 				if(err) {
 					console.log(err);
 				}
 				else {
-					console.log("The file was saved!");
+					//console.log('The file was saved!');
 				}
 			});
-			count++;
+			imageCount++;
 		});
 });
