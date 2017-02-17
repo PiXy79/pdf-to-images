179f7a28d3b11e363a4dca86ea9d7d2b05f199f8
unknown <gigio79@gmail.com>
2017-02-17 09:26:18 +0100

    Adjust Unit test on windows

--- test/test.js
+++ test/test.js
@@ -1,32 +1,41 @@
 var pdf2images = require('../lib/pdf2images.js');
 var exec = require('child_process').exec;
-const should = require('chai').should();
-const expect = require('chai').expect;
+var should = require('chai').should();
+var expect = require('chai').expect;
 
-describe('convert', function() {
-    
-    var stdout;
-    var stderr;
+describe('convert', function () {
 
-    before(function(done) {
-        exec('gs --version', function (error, stdout, stderr) {
-            if (error) {
-                done(error);
-            } 
-            stdout = stdout;
-            stderr = stderr;
-            done();
-        });
-    });
+	var stdout;
+	var stderr;
 
-    it('ghostscript is correctly installed', function(done) {
-        expect(stderr).be.empty;
-        done();
-    });
+	before(function (done) {
+		var projectPath = __dirname.split('\\');
+		projectPath.pop();
+		projectPath = projectPath.join('\\');
 
-    it('convert is defined', function(done) {
-        should.exist(pdf2images.convert);
-        pdf2images.convert.should.be.an('function');
-        done();
-    });
+		var ghostscriptPath = projectPath + '\\executables\\ghostScript';
+		// for linux compability
+		ghostscriptPath = ghostscriptPath.split('\\').join('/');
+		process.env.Path += ';' + ghostscriptPath;
+
+		exec('gs --version', function (error, stdout, stderr) {
+			if (error) {
+				done(error);
+			}
+			stdout = stdout;
+			stderr = stderr;
+			done();
+		});
+	});
+
+	it('ghostscript is correctly installed', function (done) {
+		expect(stderr).be.empty;
+		done();
+	});
+
+	it('convert is defined', function (done) {
+		should.exist(pdf2images.convert);
+		pdf2images.convert.should.be.an('function');
+		done();
+	});
 });
